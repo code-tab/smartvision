45402ca69454d371b25164f39a5f86e136bcd50b
Mohammad Tabrez Alam <54002357+code-tab@users.noreply.github.com>
2023-07-08 12:59:37 +0530

    Update ImageLinkForm.js

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -5,7 +5,7 @@ const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
   return (
     <div>
       <p className='f3'>
-        {'This SMARTVISION PREDICTION will detect faces in your pictures. Let's try.'}
+        {'This SMARTVISION PREDICTION will detect faces in your pictures. Give it a try.'}
       </p>
       <div className='center'>
         <div className='form center pa4 br3 shadow-5'>
